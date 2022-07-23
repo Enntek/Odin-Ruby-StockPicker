82b140fbc60b56cdc06084e10035ebb854e1888f
Enntek <abluecar@gmail.com>
2022-07-22 22:46:50 -0400

    Get setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Odin Ruby Project: Stock Picker
+# https://www.theodinproject.com/lessons/ruby-stock-picker
+
+This project is a classic intro to programming problem. It looks like it will 
+test our knowledge of the various iterators for both arrays and hashes.
+
+Description from Odin:
+Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
---
+++ stockpicker.rb
@@ -0,0 +1,22 @@
+# Odin Ruby stock picker
+# https://www.theodinproject.com/lessons/ruby-stock-picker
+
+def stock_picker2(array)
+
+
+end
+
+# p stock_picker([17,3,6,9,15,8,6,1,10]) # => [1, 4]
+stock_picker([17,3,6,9,15,8,6,1,10]) # no print
+
+
+
+# smaller_array = my_array.slice(1, my_array.length-1) # sub_array is next day to end of array
+# smaller_array = my_array[ 0 + 1 .. -1 ] # another way to write slices
+# p smaller_array
+
+# smaller_array = my_array.slice(-1) # last
+# smaller_array = my_array.slice(0) # first value
+# smaller_array = my_array.slice(1, 3) # slice of array
+# smaller_array = my_array[0..2] # shorter version of slice
+# smaller_array = my_array[0..-1] # start to last
